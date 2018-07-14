88bbd348abb30f58de1e2b17871823da214c659a
Kevin <brackkevin@gmail.com>
2018-07-14 14:06:16 -0500

    section 1 lesson 10

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 // Create a new component
 // This component should produce some html
 const App = () => {
   return <div>Hi!</div>;
 };
+
 // Take this component's generated html and put it
 // on the page (in the DOM)
-export default App;
+ReactDOM.render(App);
