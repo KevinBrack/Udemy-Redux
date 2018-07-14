8cf81e489342e6757b64402679e880a3afba092a
Kevin <brackkevin@gmail.com>
2018-07-14 14:53:12 -0500

    estting up env

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const YoutubeAPI = process.env.YOUTUBE_KEY;
+
 // Create a new component
 // This component should produce some html
 const App = () => {
+  console.log(YoutubeAPI);
   return <div>Hi!</div>;
 };
 
 // Take this component's generated html and put it
 // on the page (in the DOM)
-ReactDOM.render(App);
+ReactDOM.render(<App />, document.querySelector("#app"));
